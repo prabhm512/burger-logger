a325b807a56b486837d902332e2bf4691ccb7322
Prabh Singh <prabhm512@gmail.com>
2020-11-11 23:08:09 +1100

    db created and populated

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Populate table
+INSERT INTO burgers (burger_name, devoured) VALUES 
+("L'Artisan du Burger", FALSE), 
+("Block Burger", FALSE),
+("Sweet Ecstasy", FALSE),
+("Golden Eagle Spur", FALSE),
+("Three Buns", FALSE)
