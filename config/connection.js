945d431b02d59e809f70139d4780e2e7d0f48844
Prabh Singh <prabhm512@gmail.com>
2020-11-16 01:53:50 +1100

    changes made for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+const connection; 
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
   user: "root",
-  password: "mszPrabh09!",
+  password: "mszPrabh09!", //Type your MySQL password here
   database: "burgers_db",
 });
+}
 
 // Make connection
 connection.connect((err) => {
